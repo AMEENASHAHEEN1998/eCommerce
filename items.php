9603d263d5c06144e06f48dbf3dcedef39321d9c
ameena shaheen <ameena-18@hotmail.com>
2020-10-30 21:08:18 +0200

    latest

--- items.php
+++ items.php
@@ -61,14 +61,79 @@
         </div>
     </div>
     <hr class='custom-hr'>
+    <!-- start add comment -->
+<?php if(isset($_SESSION['user'])){?>
     <div class='row'>
-        <div class='col-md-3'>
-        user image
-        </div>
-        <div class='col-md-9'>
-        user comments
+        <div class='col-md-offset-3'>
+            <div class='add-comment'>
+                <h3>Add Your Comments</h3>
+                <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
+                    <textarea name="comment" id="" ></textarea>
+                    <input type="submit" class='btn btn-primary' value = 'Add Comment'>
+                </form>
+                <?php 
+                    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                        $comment = filter_var($_POST['comment'],FILTER_SANITIZE_STRING);
+                        $user_id = $_SESSION['uid'];
+                        $item_id = $row['ID'];
+
+                        if(! empty($comment)){
+                            $stmt = $con->prepare("INSERT INTO shops.comments( Comment , Status , CommentDate , Item_Id , User_Id )
+                            VALUE (:zcomment ,0 , now() , :zitemid ,:zuserid) ");
+                            $stmt->execute(array(
+                                'zcomment'=> $comment,
+                                'zitemid' => $item_id,
+                                'zuserid' => $user_id
+                            ));
+                            if($stmt){
+                                echo '<div class="alert alert-success"> Comment Add</div>';
+                            }
+                        }
+
+                        
+                    }
+                ?>
+            </div>
         </div>
     </div>
+
+    <?php } else{
+        echo '<a href="login.php">Login</a> Or <a href="login.php">Register </a>To Add Comment';
+        }?>
+
+    <!-- end add comment -->
+    <hr class='custom-hr'>
+    <?php 
+        $stmt = $con->prepare("SELECT shops.comments.* ,  shops.users.UserName AS MemberName
+        FROM shops.comments
+        INNER JOIN shops.users on shops.users.UserId = shops.comments.User_Id
+        WHERE shops.comments.Item_Id = ?
+        AND shops.comments.Status = 1
+        ORDER BY shops.comments.ID DESC");
+        //ececute query
+        $stmt->execute(array($row['ID']));
+        
+        $comments = $stmt->fetchAll();
+    ?>
+            <?php 
+                
+                foreach($comments as $comment){?>
+                    <div class='comment-box'>
+                        <div class='row'>
+                            <div class ='col-sm-2 text-center'>
+                            <img class ="img-responsive img-thumbnail img-circle center-block"src= "layout/image/personal.png" alt =""/>
+                                <?php echo $comment['MemberName'] ?></div>
+                            <div class ='col-sm-10'>
+                                <p class='lead' > <?php echo $comment['Comment'] ?></p>
+                            </div>
+                        </div>
+                        <hr class='custom-hr'>
+                    </div>
+
+                <?php }
+            ?>
+        
+    
 </div>
 
 
